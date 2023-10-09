a975c58d652f8e373aa7c839174cd8e0d0ea19e5
h1mmeister <himanshu.yadav643@gmail.com>
2023-10-09 12:51:48 +0530

    Created a fileserver to serve static content and pushed some images to static folder

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,11 +11,14 @@ import (
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
-	// mux.Use(WriteToConsole)
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	fileserver := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileserver))
+
 	return mux
 }
